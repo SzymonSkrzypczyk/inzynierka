75dfd30f5f310cccc793675065892cfd3f1ef391
Szymon Skrzypczyk <77851583+SzymonSkrzypczyk@users.noreply.github.com>
2025-07-04 11:41:51 +0200

    models are the same as the file names

--- db/models_db.py
+++ db/models_db.py
@@ -5,7 +5,6 @@ from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
 
-
 class DscovrMag1s(Base):
     __tablename__ = "dscovr_mag_1s"
     id = Column(Integer, primary_key=True, autoincrement=True)
@@ -23,15 +22,15 @@ class DscovrMag1s(Base):
     phi_gsm = Column(Float)
 
 
-class F107Flux(Base):
-    __tablename__ = "f10_7cm_flux"
+class F107cmFlux(Base):
+    __tablename__ = "f10-7cm-flux"
     id = Column(Integer, primary_key=True, autoincrement=True)
     time_tag = Column(DateTime, unique=True, index=True)
-    f107 = Column("f10.7", Float)
+    f10_7 = Column("f10.7", Float)
 
 
 class Magnetometers1Day(Base):
-    __tablename__ = "magnetometers_1_day"
+    __tablename__ = "magnetometers-1-day"
     id = Column(Integer, primary_key=True, autoincrement=True)
     time_tag = Column(DateTime)
     satellite = Column(String)
@@ -47,15 +46,15 @@ class Magnetometers1Day(Base):
 
 
 class ObservedSolarCycleIndices(Base):
-    __tablename__ = "observed_solar_cycle_indices"
+    __tablename__ = "observed-solar-cycle-indices"
     id = Column(Integer, primary_key=True, autoincrement=True)
     time_tag = Column(DateTime, unique=True, index=True)
     ssn = Column(Float)
     smoothed_ssn = Column(Float)
     observed_swpc_ssn = Column(Float)
     smoothed_swpc_ssn = Column(Float)
-    f107 = Column("f10.7", Float)
-    smoothed_f107 = Column("smoothed_f10.7", Float)
+    f10_7 = Column("f10.7", Float)
+    smoothed_f10_7 = Column("smoothed_f10.7", Float)
 
 
 class PlanetaryKIndex1m(Base):
@@ -68,7 +67,7 @@ class PlanetaryKIndex1m(Base):
 
 
 class PredictedSolarCycle(Base):
-    __tablename__ = "predicted_solar_cycle"
+    __tablename__ = "predicted-solar-cycle"
     id = Column(Integer, primary_key=True, autoincrement=True)
     time_tag = Column(DateTime, unique=True, index=True)
     predicted_ssn = Column(Float)
@@ -78,46 +77,55 @@ class PredictedSolarCycle(Base):
     low25_ssn = Column(Float)
     low_ssn = Column(Float)
     low75_ssn = Column(Float)
-    predicted_f107 = Column(Float)
-    high25_f107 = Column(Float)
-    high_f107 = Column(Float)
-    high75_f107 = Column(Float)
-    low25_f107 = Column(Float)
-    low_f107 = Column(Float)
-    low75_f107 = Column(Float)
+    predicted_f10_7 = Column("predicted_f10.7", Float)
+    high25_f10_7 = Column("high25_f10.7", Float)
+    high_f10_7 = Column("high_f10.7", Float)
+    high75_f10_7 = Column("high75_f10.7", Float)
+    low25_f10_7 = Column("low25_f10.7", Float)
+    low_f10_7 = Column("low_f10.7", Float)
+    low75_f10_7 = Column("low75_f10.7", Float)
 
 
-class ParticleMeasurement(Base):
-    __tablename__ = "particle_measurements"
+class PrimaryDifferentialElectrons1Day(Base):
+    __tablename__ = "primary-differential-electrons-1-day"
     id = Column(Integer, primary_key=True, autoincrement=True)
     time_tag = Column(DateTime)
     satellite = Column(String)
     flux = Column(Float)
     energy = Column(Float)
-    particle_type = Column(Enum("electron", "proton", name="particle_type_enum"))
-    aggregation = Column(Enum("differential", "integral", name="aggregation_type_enum"))
-    satellite_type = Column(Enum("primary", "secondary", name="satellite_type_enum"))
-
-    __table_args__ = (
-        UniqueConstraint("time_tag", "satellite", "particle_type", "aggregation", "satellite_type", name="uq_particle_measurements"),
-    )
 
 
-class ProtonDifferentialExtra(Base):
-    __tablename__ = "proton_differential_extras"
+class PrimaryDifferentialProtons1Day(Base):
+    __tablename__ = "primary-differential-protons-1-day"
     id = Column(Integer, primary_key=True, autoincrement=True)
     time_tag = Column(DateTime)
     satellite = Column(String)
+    flux = Column(Float)
+    energy = Column(Float)
     yaw_flip = Column(Boolean)
     channel = Column(String)
 
-    __table_args__ = (
-        UniqueConstraint("time_tag", "satellite", name="uq_proton_extras"),
-    )
+
+class PrimaryIntegralElectrons1Day(Base):
+    __tablename__ = "primary-integral-electrons-1-day"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    time_tag = Column(DateTime)
+    satellite = Column(String)
+    flux = Column(Float)
+    energy = Column(Float)
 
 
-class XRayMeasurement(Base):
-    __tablename__ = "xray_measurements"
+class PrimaryIntegralProtons1Day(Base):
+    __tablename__ = "primary-integral-protons-1-day"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    time_tag = Column(DateTime)
+    satellite = Column(String)
+    flux = Column(Float)
+    energy = Column(Float)
+
+
+class PrimaryXray1Day(Base):
+    __tablename__ = "primary-xray-1-day"
     id = Column(Integer, primary_key=True, autoincrement=True)
     time_tag = Column(DateTime)
     satellite = Column(String)
@@ -126,22 +134,67 @@ class XRayMeasurement(Base):
     electron_correction = Column(Float)
     electron_contaminaton = Column(Float)
     energy = Column(String)
-    satellite_type = Column(Enum("primary", "secondary", name="xray_satellite_type_enum"))
-
-    __table_args__ = (
-        UniqueConstraint("time_tag", "satellite", "satellite_type", name="uq_xray_measurements"),
-    )
 
 
-class SatelliteLongitude(Base):
-    __tablename__ = "satellite_longitudes"
+class SatelliteLongitudes(Base):
+    __tablename__ = "satellite-longitudes"
     id = Column(Integer, primary_key=True, autoincrement=True)
     satellite = Column(String, unique=True)
     longitude = Column(Float)
 
 
+class SecondaryDifferentialElectrons1Day(Base):
+    __tablename__ = "secondary-differential-electrons-1-day"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    time_tag = Column(DateTime)
+    satellite = Column(String)
+    flux = Column(Float)
+    energy = Column(Float)
+
+
+class SecondaryDifferentialProtons1Day(Base):
+    __tablename__ = "secondary-differential-protons-1-day"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    time_tag = Column(DateTime)
+    satellite = Column(String)
+    flux = Column(Float)
+    energy = Column(Float)
+    yaw_flip = Column(Boolean)
+    channel = Column(String)
+
+
+class SecondaryIntegralElectrons1Day(Base):
+    __tablename__ = "secondary-integral-electrons-1-day"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    time_tag = Column(DateTime)
+    satellite = Column(String)
+    flux = Column(Float)
+    energy = Column(Float)
+
+
+class SecondaryIntegralProtons1Day(Base):
+    __tablename__ = "secondary-integral-protons-1-day"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    time_tag = Column(DateTime)
+    satellite = Column(String)
+    flux = Column(Float)
+    energy = Column(Float)
+
+
+class SecondaryXray1Day(Base):
+    __tablename__ = "secondary-xray-1-day"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    time_tag = Column(DateTime)
+    satellite = Column(String)
+    flux = Column(Float)
+    observed_flux = Column(Float)
+    electron_correction = Column(Float)
+    electron_contaminaton = Column(Float)
+    energy = Column(String)
+
+
 class SolarRadioFlux(Base):
-    __tablename__ = "solar_radio_flux"
+    __tablename__ = "solar-radio-flux"
     id = Column(Integer, primary_key=True, autoincrement=True)
     time_tag = Column(DateTime)
     common_name = Column(String)
@@ -152,7 +205,7 @@ class SolarRadioFlux(Base):
     )
 
 
-class SolarRegion(Base):
+class SolarRegions(Base):
     __tablename__ = "solar_regions"
     id = Column(Integer, primary_key=True, autoincrement=True)
     observed_date = Column(DateTime)
